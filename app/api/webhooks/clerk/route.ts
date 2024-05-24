7580216c2a8bcac16ff291c6afdf7041cb70d054
Bilal Khan <bilal.khan@live.com>
2024-05-24 18:52:38 +1000

    Troubleshooting depcrecated middleware clerkClient

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,9 +1,10 @@
 /* eslint-disable camelcase */
-import { clerkClient } from "@clerk/nextjs";
+import { clerkClient } from "@clerk/nextjs/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
+
 
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
