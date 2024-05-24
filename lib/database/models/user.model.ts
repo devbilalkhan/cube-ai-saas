8498ca752cc2981cdafa5e65c767d94683bccd6b
Bilal Khan <bilal.khan@live.com>
2024-05-24 19:13:54 +1000

    change the types for userparams

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,27 +1,12 @@
 import { Schema, models, model, Document } from "mongoose";
 
-
-export interface IUser extends Document {
-  clerkId: string;
-  email: string;
-  username: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  photo: string;
-  planId: string;
-  creditBalance: number;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true},
   email: { type: String, required: true, unique: true},
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String},
   photo: { type: String, required: true },
   planId: { type: String, default: 'free' },
   creditBalance: { type: Number, default: 50 },
